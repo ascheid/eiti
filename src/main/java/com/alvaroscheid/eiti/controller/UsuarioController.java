3881d965b2ed28b079f96f8fb49f7d3c1d699897
Alvaro Scheid <ascheid@gmail.com>
2018-07-07 13:18:04 -0300

    Controller Implementation

---
+++ src/main/java/com/alvaroscheid/eiti/controller/UsuarioController.java
@@ -0,0 +1,12 @@
+package com.alvaroscheid.eiti.controller;
+
+/**
+ * 
+ *
+ * @author Alvaro Scheid | ascheid@gmail.com - 7 de jul de 2018
+ */
+@Controller
+public class UsuarioController {
+
+	@
+}
