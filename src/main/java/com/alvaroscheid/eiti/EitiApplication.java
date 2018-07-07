00edbea7fea7f1e8fcc491ebf20c8057c1e1be32
Alvaro Scheid <ascheid@gmail.com>
2018-07-07 12:37:48 -0300

    first commit - base phase

---
+++ src/main/java/com/alvaroscheid/eiti/EitiApplication.java
@@ -0,0 +1,12 @@
+package com.alvaroscheid.eiti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EitiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EitiApplication.class, args);
+	}
+}
